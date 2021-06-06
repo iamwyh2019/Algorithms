02c3d95a4aa35181191a9a6b24271902f4e7b558
iamwyh2019 <799810767@qq.com>
2021-06-06 18:24:31 +0800

    Create DoublyLinkedListTheoryPropertyFirst.java

---
+++ src/main/java/com/williamfiset/algorithms/datastructures/linkedlist/DoublyLinkedListTheoryPropertyFirst.java
@@ -0,0 +1,18 @@
+package com.williamfiset.algorithms.datastructures.linkedlist;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runners.Parameterized.Parameter;
+import static org.junit.jupiter.api.Assumptions.*;
+
+public class DoublyLinkedListTheoryPropertyFirst {
+
+}
